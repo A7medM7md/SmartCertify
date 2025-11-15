f7fd10aeeea57d2ad17046799853f0ff05823cfb
A7medM7md <ahmedelgebaly2511@gmail.com>
2025-11-15 14:21:04 +0200

    setup header, footer, home, and about components

---
+++ frontend/smartcertify-angular/src/app/pages/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  // templateUrl: './about.component.html',
+  template: `<h1>Ahmed</h1>`,
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
